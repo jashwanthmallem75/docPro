02f8723208fb78e1770b969b36f34d3e5ffe87cf
Mallem jashwanth <mallemjashwanth75@gmail.com>
2026-04-14 17:35:54 +0530

    update build with loaddata

--- build.sh
+++ build.sh
@@ -4,13 +4,10 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py loaddata core/fixtures/initial_data.json
 
-# Create superuser if not exists
 echo "
 from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'demoformydjangoproj@gmail.com', 'admin123')
-    print('Superuser created')
-else:
-    print('Superuser already exists')
+if not User.objects.filter(username='mallem').exists():
+    User.objects.create_superuser('mallem', 'demoformydjangoproj@gmail.com', 'mallem')
 " | python manage.py shell
